2e9d323805bc217897333e28472d5690541a337b
OriishiTakahiro <takahiro0914@live.jp>
2020-01-08 22:15:15 +0900

    Various network type

--- network-generator/README.md
+++ network-generator/README.md
@@ -1,9 +1,18 @@
 # Network Generator
 
+This tool generates a YAML file which is specified a network.
 
-## with Docker
+This tool can generates some kinds network.
+
+- random graph
+- full mesh graph
+- path grpah
+- tree grpah
+
+## Usage
 ```sh
-# docker build netgen
-# doker run netgen -v $PWD:/root/artifacts
+$ docker build netgen -t .
+# Generates path graph (5 nodes, docker image 'hoge')
+$ docker run  -v $PWD:/root/artifacts netgen path -n 5 -i hoge
 ```
 
--- network-generator/src/main.py
+++ network-generator/src/main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+import sys
 import argparse
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -9,24 +12,65 @@ PREFIX_SPACE    = "2001:0db8:1b:ff{prefix}::/64"
 ADDR_SPACE      = "2001:0db8:1b:ff{prefix}::ff{last_oct}/64"
 ROUTER_ID_SPACE = "10.0.0.{node_id}"
 
+ARTIFACT_PATH   = "/root/artifacts"
+
+# handler functions for create a graph
+def create_random_graph(args):
+    return nx.fast_gnp_random_graph(args.nodes, args.proverbility)
+
+def create_full_mesh_graph(args):
+    return nx.fast_gnp_random_graph(args.nodes, 1)
+
+def create_path_graph(args):
+    return nx.path_graph(args.nodes)
+
+def create_tree_graph(args):
+    return nx.random_tree(args.nodes)
+
 # check paramters
-parser = argparse.ArgumentParser(description='network parameters.')
-parser.add_argument('--nodes', metavar='-n', type=int, help='Number of nodes.', default=64)
-parser.add_argument('--proverbility', metavar='-p', type=float, help='Proverbility of make edges between 2 nodes.', default=0.08)
-parser.add_argument('--yaml', metavar='-y', type=str, help='File name of YAML.', default='network.yml')
-parser.add_argument('--figure', metavar='-f', type=str, help='File name of graph figure.', default='graph.png')
-parser.add_argument('--image', metavar='-i', type=str, help='Docker image.', default='quagga-ca')
+parser = argparse.ArgumentParser(description="Auto network topology generator.")
+subparsers = parser.add_subparsers(dest="graph_type")
+subparsers.required = True
+
+# subcommand for random graph
+parser_random = subparsers.add_parser("random", help="Create a random graph.")
+parser_random.add_argument("-n", "--nodes", type=int, help="Number of nodes.", default=64)
+parser_random.add_argument("-p", "--proverbility", type=float, help="Proverbility of make edges between 2 nodes.", default=0.08)
+parser_random.add_argument("-i", "--image", type=str, help="Docker image.", default="quagga-ca")
+parser_random.set_defaults(handler=create_random_graph)
+
+# subcommand for full mesh graph
+parser_full_mesh = subparsers.add_parser("fullmesh", help="Create a full mesh graph.")
+parser_full_mesh.add_argument("-n", "--nodes", type=int, help="Number of nodes.", default=64)
+parser_full_mesh.add_argument("-i", "--image", type=str, help="Docker image.", default="quagga-ca")
+parser_full_mesh.set_defaults(handler=create_full_mesh_graph)
+
+# subcommand for path graph
+parser_path = subparsers.add_parser("path", help="Create a path graph.")
+parser_path.add_argument("-n", "--nodes", type=int, help="Number of nodes.", default=64)
+parser_path.add_argument("-i", "--image", type=str, help="Docker image.", default="quagga-ca")
+parser_path.set_defaults(handler=create_path_graph)
+
+# subcommand for tree graph
+parser_tree = subparsers.add_parser("tree", help="Create a tree graph.")
+parser_tree.add_argument("-n", "--nodes", type=int, help="Number of nodes.", default=64)
+parser_tree.add_argument("-i", "--image", type=str, help="Docker image.", default="quagga-ca")
+parser_tree.set_defaults(handler=create_tree_graph)
+
 args = parser.parse_args()
 
+# execute handler function
+graph = args.handler(args)
+
 # create graph
-graph = nx.fast_gnp_random_graph(args.nodes, args.proverbility)
+# graph = nx.fast_gnp_random_graph(args.nodes, args.proverbility)
 
 nodes = {}
 
 # create nodes
 for n in graph.nodes():
     name = "node" +  str(n)
-    prefixes = [ PREFIX_SPACE.format(prefix = format(n, '02x')) ]
+    prefixes = [ PREFIX_SPACE.format(prefix = format(n, "02x")) ]
     node_config = {
             "name": name,
             "image": args.image,
@@ -40,38 +84,34 @@ for n in graph.nodes():
         }
     nodes[n] = node_config
 
-# set random node to CA
-nodes[randint(0, len(nodes))]["is_ca"] = True
+# set firs node as CA
+nodes[0]["is_ca"] = True
 
 # create switches
 for e in graph.edges(data=True):
     # start node
     nodes[e[0]]["interfaces"].append( {
-        "name": "eth" + format(e[0], '03d') + format(e[1], '03d'),
-        "prefix": ADDR_SPACE.format(prefix = format(e[0], '02x'), last_oct = format(e[1], '02x')),
+        "name": "eth" + format(e[0], "03d") + format(e[1], "03d"),
+        "prefix": ADDR_SPACE.format(prefix = format(e[0], "02x"), last_oct = format(e[1], "02x")),
         "area": "0.0.0.0",
-        "switch": "s" + format(e[0], '03d') + format(e[1], '03d'),
+        "switch": "s" + format(e[0], "03d") + format(e[1], "03d"),
         "cost": "1",
         "priority": "1",
     })
     # end node
     nodes[e[1]]["interfaces"].append( {
-        "name": "eth" + format(e[1], '03d') + format(e[0], '03d'),
-        "prefix": ADDR_SPACE.format(prefix = format(e[1], '02x'), last_oct = format(e[0], '02x')),
+        "name": "eth" + format(e[1], "03d") + format(e[0], "03d"),
+        "prefix": ADDR_SPACE.format(prefix = format(e[1], "02x"), last_oct = format(e[0], "02x")),
         "area": "0.0.0.0",
-        "switch": "s" + format(e[0], '03d') + format(e[1], '03d'),
+        "switch": "s" + format(e[0], "03d") + format(e[1], "03d"),
         "cost": "1",
         "priority": "1",
     })
 
-nodes_list = []
-for v in nodes.values():
-    nodes_list.append(v)
-
 # save YAML file
-with open(args.yaml, "w") as f:
-    f.write(dump(nodes_list))
+with open( "{dir}/{type}-{image}-{nodes}.yaml".format(dir=ARTIFACT_PATH, type=args.graph_type, image=args.image, nodes=args.nodes)  , "w") as f:
+    f.write(dump( list(nodes.values()) ))
 
 # save network graph
 nx.draw_networkx(graph)
-plt.savefig(args.figure)
+plt.savefig( "{dir}/{type}-{image}-{nodes}.svg".format(dir=ARTIFACT_PATH, type=args.graph_type, image=args.image, nodes=args.nodes), format="svg" )
